3b8f6dc7c6a91d0c7269883c5c3a1ae1a445a937
Alina Leuca <alinaleuca@gmail.com>
2017-12-27 17:28:43 +0400

    fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+
+
+
+for (let i = 0; i <= number; i++) {
+    if (i % 3 === 0) {
+        console.log ("fizz")
+    }
+    if (i % 5 === 0) {
+        console.log("buzz")
+    }
+    if (i % 15 === 0) {
+        console.log("fizzbuzz")
+    } else {
+        console.log("none of these")
+    }
+}
+
